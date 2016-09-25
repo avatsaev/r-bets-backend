9e9aef074975116ecbe9ae9f2b640e0328e16d5f
A. Vatsaev <azero79@gmail.com>
2016-09-25 23:56:34 +0200

    compile prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
 
   def health_check
-    render json: {}, status: :success
+    render json: {
+        EC2_REGION: ENV['EC2_REGION'],
+        instance_id: ENV['EC2_INSTANCE_ID'],
+        avail_zone: ENV['EC2_AVAIL_ZONE']
+    }, status: :ok
   end
 end
