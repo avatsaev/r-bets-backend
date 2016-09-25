0800aeee840efa0ba5bdeabb4c604a14898acf2e
A. Vatsaev <azero79@gmail.com>
2016-09-26 00:44:46 +0200

    compile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::API
     render json: {
         EC2_REGION: ENV['EC2_REGION'],
         instance_id: ENV['EC2_INSTANCE_ID'],
-        avail_zone: ENV['EC2_AVAIL_ZONE']
+        avail_zone: ENV['EC2_AVAIL_ZONE'],
+        host: request.host
     }, status: :ok
   end
 end
